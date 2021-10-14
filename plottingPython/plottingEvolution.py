62bd9ff120c0e05d60151e52c975d0a42b75ca0f
alexanderepping <67109067+alexanderepping@users.noreply.github.com>
2021-10-14 16:48:40 +0200

    updated plottingEvolution.py to include the flavor

--- plottingPython/plottingEvolution.py
+++ plottingPython/plottingEvolution.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # import the data
-x, apfel, lhapdf = np.loadtxt(open("data_Evolution.txt"), delimiter="	", unpack=True)
+particle = np.genfromtxt('data_Evolution.txt',dtype='str', max_rows=1)
+ylabel = str(particle) + " PDF"
+x, apfel, lhapdf = np.loadtxt(open("data_Evolution.txt"), delimiter="	", unpack=True, skiprows=1)
 
 # delete the zeros out of the 
 apfel2 = np.array([])
@@ -11,11 +13,15 @@ for i in range(len(apfel)):
     if apfel[i]!=0:
         apfel2 = np.append(apfel2, apfel[i])
         x2 = np.append(x2, x[i])
-    
+
 plt.plot(x2, apfel2, label="apfel values")
 plt.plot(x, lhapdf, label="lhapdf values")
 
+plt.xscale('log')
+
 plt.xlabel("x")
+plt.ylabel(ylabel)
 plt.legend(loc="upper right")
-plt.ylim(top=1, bottom=0)
+#plt.ylim(top=1, bottom=0)
+#plt.xlim(right=1, left=10**(-5))
 plt.show()
