9293e6ae506d2092be08f6a18b9d5f5e32d863c2
alexanderepping <epping.alexander@gmx.de>
2021-10-28 13:15:53 +0200

    added makeLHAPDFHO.f and small changes - makeLHAPDFHO.f: similar to makeLHAPDFLO.f but for higher order terms. - makeLHAPDFLO.f: removed some errors that came up because of name change - README.md: included new files

--- makeLHAPDF/README.md
+++ makeLHAPDF/README.md
@@ -9,14 +9,21 @@ Then it reads the x- (4th line) and Q-values (5th line) and saves them.
 They are then used with the GRVGLO subroutine to calculate the Photon PDF values at different x- and Q²-values. 
 These values are then written to the output file.
 
-The program can be run by running the bashFiles/makeLHAPDFLO.sh program, which will also copy the GRVCustomSetLO_0000.dat directly into the LHAPDF share directory.
+The program can be run by running the bashFiles/makeLHAPDFLO.sh program, which will also copy the GRVCustomSetLO_0000.dat directly into the LHAPDF share directory and delete the executable as well as the .dat file in here.
+
+## makeLHAPDFLO.f
+Program to write a GRVCustomSetHO_0000.dat file in the LHAPDF fromat.
+
+The program takes a template file and copies the first three lines.
+Then it reads the x- (4th line) and Q-values (5th line) and saves them.
+They are then used with the subroutines from the grvphoton.f program to calculate the Photon PDF values at different x- and Q²-values.
+These values are then written to the output file.
+You can change if you want to add for example the LO terms by changing the integer "mode".
+
+The program can be run by running the bashFiles/makeLHAPDFHO.sh program, which will also copy the GRVCustomSetHO_0000.dat directly into the LHAPDF share directory and delete the executable as well as the .dat file in here.
 
 ## grvphoton.f
 Program by GRV containing the GRVGLO subroutine which calculates the PDFs at different x- and Q²-values.
 
 ## LHAPDF_template.dat
 Template for the LHAPDF format.
-
-## GRVCustomSetLO_0000.dat
-GRV Photon PDF at different x- and Q²-values in the LHAPDF format.
-It is output by the makeLHAPDF.f file and is readable by the LHAPDF library.
--- makeLHAPDF/makeLHAPDFHO.f
+++ makeLHAPDF/makeLHAPDFHO.f
@@ -1,17 +1,19 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*  makeLHAPDFLO.f                                                                                                   *
+*  makeLHAPDFHO.f                                                                                                   *
 *  Author: Alexander Epping: a_eppi01@uni-muenster.de                                                               *
-*  6 Oct 2021                                                                                                       *         
+*  25 Oct 2021                                                                                                      *         
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*  Program to write a GRVCustomSetLO_0000.dat file in the LHAPDF fromat.                                            *            
+*  Program to write a GRVCustomSetHO_0000.dat file in the LHAPDF fromat.                                            *            
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 *  The program takes a template file and copies the first three lines.                                              *
 *  Then it reads the x- (4th line) and Q-values (5th line) and saves them.                                          *
-*  They are then used with the GRVGLO subroutine to calculate the Photon PDF values at different x- and Q²-values.  *
+*  They are then used with the subroutines from the grvphoton.f program to calculate the Photon PDF values at       *
+*  different x- and Q²-values.                                                                                      *
 *  These values are then written to the output file.                                                                *
+*  You can change if you want to add for example the LO terms by changing the integer "mode".                       *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-      program makeLHAPDFLO 
+      program makeLHAPDFHO 
         implicit none
 
 c       file, which has the same X- and Q-values as the one we want to create    
@@ -19,7 +21,7 @@ c       the file should be in a LHAPDF format
         character(len=19) :: template_file = 'LHAPDF_template.dat'
 
 c       file, which we want to create in the LHAPDF format
-        character(len=23) :: output_file = 'GRVCustomSetLO_0000.dat'
+        character(len=23) :: output_file = 'GRVCustomSetHO_0000.dat'
 
         integer :: lenXArr, lenQArr
         character(len=99999) :: lineOne, lineTwo, lineThree
@@ -27,14 +29,24 @@ c       file, which we want to create in the LHAPDF format
 
         real, allocatable,dimension(:) :: XArr, QArr
 
-c       variables that are allocated inside the do-loops/by the GRVGLO-subroutine
-        real :: X, Q2, UL, DL, SL, CL, BL, GL    
+c       variables that will save the LO PDFs
+        real :: UL, DL, SL, CL, BL, GL   
+
+c       variables that will save the HO PDFs
+        real :: X, Q2, UH, DH, SH, CH, BH, GH  
+
+c       variables that will save the correctins to the PDFs 
+        real :: U0, D0, S0, C0, B0, G0    
 
 c       running index variables for do-loops
         integer :: i, j
 
 c       Structure constant
         real :: alphaQED = 1./137.
+
+c       dictates which parts are added together:
+c       1: H, 2: H+L, 3: H+0, 4: H+L+0
+        integer :: mode = 1
 
 
 
@@ -89,16 +101,54 @@ c - open the output file and write the first lines
 
 c - run the loop, write all the data and close the file
         do i = 1, lenXArr
+          !write(*,*) XArr(i)!debug
+
           do j = 1, lenQArr
 
             X = XArr(i)
             Q2 = QArr(j)*QArr(j)
 
-c           call the subroutine from grvphoton.f
+c           call the GRVGLO subroutine from grvphoton.f
             call GRVGLO (X, Q2, UL, DL, SL, CL, BL, GL)
 
-            write(2,"(10(es13.6, 1x), es13.6)") BL, CL, SL, UL, DL, DL, 
-     >       UL, SL, CL, BL, GL
+c           call the GRVGHO subroutine from grvphoton.f
+            call GRVGHO (X, Q2, UH, DH, SH, CH, BH, GH)
+
+c           call the GRVGH0 subroutine from grvphoton.f
+            call GRVGH0 (X, Q2, U0, D0, S0, C0, B0, G0)
+
+
+            !write(*,*) SL, SH, S0!debug
+
+
+c           add the corrections:
+            if (mode .EQ. 2) then
+                UH = UH + UL
+                DH = DH + DL
+                SH = SH + SL
+                CH = CH + CL
+                BH = BH + BL
+                GH = GH + GL
+            else if (mode .EQ. 3) then
+                UH = UH + U0
+                DH = DH + D0
+                SH = SH + S0
+                CH = CH + C0
+                BH = BH + B0
+                GH = GH + G0
+            else if (mode .EQ. 4) then
+                UH = UH + U0 + UL
+                DH = DH + D0 + DL
+                SH = SH + S0 + SL
+                CH = CH + C0 + CL
+                BH = BH + B0 + BL
+                GH = GH + G0 + GL
+            end if
+
+
+            write(2,"(10(es13.6, 1x), es13.6)") BH, CH, SH, UH, DH, DH, 
+     >       UH, SH, CH, BH, GH
+
           end do
         end do 
 
@@ -111,4 +161,4 @@ c       write the last line which is the same as the third
 c - deallocate the memory of the arrays
         deallocate(QArr, XArr)
 
-      end program makeLHAPDFLO
+      end program makeLHAPDFHO
